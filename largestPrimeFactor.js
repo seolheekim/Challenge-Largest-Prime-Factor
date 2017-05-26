28d9f5c662f73e4a2c445e09397a2ae33d1aa53e
Seolhee Kim <seolhee@upspringmedia.com>
2017-05-25 16:17:10 -1000

    primeFactor

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,16 @@
 exports.largestPrimeFactor = function(n){
-  var primeNumber = 0;
-  // do your work here
+  var primeNumber = 2;
+
+    while(primeNumber < n){
+      //when n is divided by even number
+      if(n % primeNumber === 0){
+        //divide the primeNumber with n
+        n /= primeNumber;
+      }else{
+        primeNumber++;
+      }
+    }
+
 
   return primeNumber;
 };
